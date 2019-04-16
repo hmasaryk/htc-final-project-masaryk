931abd6278c574a78a953f596836eaacb2d33983
User <User@DEV-022782.OnBase.net>
2019-04-16 17:57:15 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace htc_final_project_masaryk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
